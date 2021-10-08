0e08de035d9c7b4d2d3658c735902bbaba8840a6
quylian <1029659@hr.nl>
2021-10-08 15:02:02 +0200

    game over

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import pygame
 from rocket import Rocket
 from asteroid import asteroid
+from omgeving import highscore_button
+from omgeving import retry_button
+from omgeving import close_button
+
 
 pygame.font.init()
 
@@ -13,6 +17,8 @@ pygame.display.set_caption("First Game!")
 FPS = 60
 
 SCORE_FONT = pygame.font.SysFont('commicsans', 40)
+SCORE_FONT_ELSE = pygame.font.SysFont('commicsans', 60)
+GAME_OVER = pygame.font.SysFont("comicsans" , 100)
 
 ASTEROID_L_WIDTH, ASTEROID_L_HEIGHT = 50, 50
 ASTEROID_M_WIDTH, ASTEROID_M_HEIGHT = 35, 35
@@ -26,12 +32,23 @@ asteroid_s = pygame.Rect(300, 0, ASTEROID_S_WIDTH, ASTEROID_S_HEIGHT)
 def draw(rocket, score):
     win.blit(backGround, (0,0))
     rocket.draw(win)
-    score_text = SCORE_FONT.render("score: " + str(score), 1, (255, 255, 0))
-    win.blit(score_text, (width  - score_text.get_width() - 10, 10))
     asteroid.draw_window(asteroid_l, asteroid_m, asteroid_s, win)
+    if hp >= 1:
+        score_text = SCORE_FONT.render("score: " + str(score), 1, (255, 255, 0))
+        win.blit(score_text, (width  - score_text.get_width() - 10, 10))
+    else:
+        score_text = SCORE_FONT_ELSE.render("score: " + str(score), 1, (255, 255, 0))
+        win.blit(score_text, (width - 450 , 150))
+        game_over_text = GAME_OVER.render("game over", 1, (255, 0 , 0))
+        win.blit(game_over_text, (width - 530 , 75))
+        win.blit(highscore_button, ( 200 , 250))
+        win.blit(retry_button, ( 200 , 200))
+        win.blit(close_button, ( 200 , 300))
+
     pygame.display.update()
 
 score = 5
+hp = 0
 
 def main():
     rocket = Rocket()
