16108e82280a4fdb4245df1e7255cf2bf9fe9f66
quylian <1029659@hr.nl>
2021-10-12 14:07:49 +0200

    button detect

--- omgeving.py
+++ omgeving.py
@@ -1,13 +1,13 @@
 import pygame
 
 highscore = pygame.image.load('assets/highscore_button.png')
-highscore_width, highscore_length = 600, 400
+highscore_width, highscore_length = 240, 75
 highscore_button = pygame.transform.scale( highscore, (highscore_width, highscore_length))
 
 retry = pygame.image.load('assets/retry.png')
-retry_width, retry_length = 600, 400
+retry_width, retry_length = 240, 75
 retry_button = pygame.transform.scale( retry, (retry_width, retry_length))
 
 close = pygame.image.load('assets/close.png')
-close_width, close_length = 600, 400
+close_width, close_length = 240, 75
 close_button = pygame.transform.scale( close, (close_width, close_length))
