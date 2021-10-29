0fe6ecb1760bdc4af9336345254490893e6ed412
Babette van Aubel <babettevanaubel@gmail.com>
2021-10-29 17:28:09 +0200

    final version of the scores screen

--- scores_screen.py
+++ scores_screen.py
@@ -1,8 +1,8 @@
 import pygame, os
-import highscore_buttons
 import game
 import json
-
+import home_screen
+from rocket import playerRocket
 
 # start game
 pygame.init()
@@ -13,32 +13,23 @@ window = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Score Screen!")
 
 # adding a background image
-background = pygame.image.load(os.path.join("assets/scores_bg.png"))
+background = pygame.image.load(os.path.join("assets/background.png"))
 background = pygame.transform.scale(background, (1280, 720))
 
+# title game
+title = pygame.image.load(os.path.join("assets", "title_scores.png"))
+title = pygame.transform.scale(title, (400, 280))
+
+# title position
+title_rect = title.get_rect()
+title_rect.center = (window.get_width() / 2, title_rect.height / 2.65)
+title.get_width()
+
 # load buttons
-update_score = pygame.image.load(os.path.join("assets/update_btn.png")).convert_alpha()
-update_score = pygame.transform.scale(update_score, (250, 80))
 retry = pygame.image.load(os.path.join("assets/retry_btn.png")).convert_alpha()
-retry = pygame.transform.scale(retry, (199, 60))
-quit_game = pygame.image.load(os.path.join("assets/quit_btn3.png"))
-quit_game = pygame.transform.scale(quit_game, (199, 60))
-
-
-def check_for_new_highscore():
-    # read the json file
-    with open("mydata.json", "r") as jsondatafile:
-        highscore_json = jsondatafile.readlines()
-        for each in highscore_json:
-            print(each)
-
-    return each
-
-
-# score display
-score = check_for_new_highscore()
-SCORE_FONT = pygame.font.SysFont("commicsans", 80)
-score_text = SCORE_FONT.render("Score: " + str(score), 1, (255, 255, 255))
+retry = pygame.transform.scale(retry, (210, 60))
+quit_game = pygame.image.load(os.path.join("assets/quit_btn.png"))
+quit_game = pygame.transform.scale(quit_game, (210, 60))
 
 # button class
 class Button:
@@ -70,20 +61,10 @@ class Button:
 
 
 # create instance of buttons
-retry_button = Button(window.get_width() / 1.35, retry.get_rect().height / 0.13, retry)
+retry_button = Button(window.get_width() / 2.35, retry.get_rect().height / 0.13, retry)
 quit_button = Button(
-    window.get_width() / 1.35, quit_game.get_rect().height / 0.11, quit_game
+    window.get_width() / 2.35, quit_game.get_rect().height / 0.11, quit_game
 )
-update_button = Button(
-    window.get_width() / 3.15, update_score.get_rect().height / 0.30, update_score
-)
-
-
-def show_score():
-    SCORE_FONT = pygame.font.SysFont("commicsans", 120)
-    # score_text = SCORE_FONT.render("Score: " + str(score), 1, (255, 255, 255))
-    # window.blit(score_text, (width / 2 - score_text.get_width() - 10, 300))
-
 
 # game loop
 def main():
@@ -92,22 +73,25 @@ def main():
     while running:
         # drawing images on screen
         window.blit(background, (0, 0))
-        # window.blit(score_text, (0, 0))
-        # score_text.set_alpha(0)
+        window.blit(title, title_rect)
+        # with the open statement, load the updata json file
+        with open("mydata.json") as json_file:
+            data = json.load(json_file)
+            # positioning score text
+            SCORE_FONT = pygame.font.SysFont("commicsans", 80)
+            score_text = SCORE_FONT.render(
+                "Your New Score: " + str(data), 1, (255, 255, 255)
+            )
+            # display score text
+            window.blit(score_text, (width - score_text.get_width() - 350, 300))
 
         # set buttons
         if retry_button.draw():
-            game.main()
+            game.main(difficulty=1)
+            playerRocket.set_alpha(100)
             print("retry clicked")
-        if show_score():
-            score = check_for_new_highscore()
-        if update_button.draw():
-            print("update clicked")
-            score = check_for_new_highscore()
-            show_score()
-            window.blit(score_text, (width / 2 - score_text.get_width(), 350))
         if quit_button.draw():
-            running = False
+            home_screen.main()
             print("quit clicked")
         # event handler
         for event in pygame.event.get():
